04d3e1c897f86bb06920e09619751d1b5ba577bf
Weronika Win <w1nw3r25@gmail.com>
2021-03-21 15:26:18 +0100

    Full shopping_list

--- lista_zakupow2.py
+++ lista_zakupow2.py
@@ -1,5 +1,6 @@
 print("Lista zakupów")
 lista_zakupow = ["Chleb bezglutenowy", "Bułki" ,"Rogaliki","Kokosanki","Beza","Tort"]
 shopping_dict = {
-    
+    "piekarnia" : ["Chleb bezglutenowy", "Bułki" ,"Rogaliki"],
+    "cukiernia" : ["Kokosanki","Beza","Tort"]
 }
