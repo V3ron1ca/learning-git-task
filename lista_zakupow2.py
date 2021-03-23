36a43d134dbb141f8e3fbd7a13343a4845612957
Weronika Win <w1nw3r25@gmail.com>
2021-03-23 02:19:11 +0100

    Change meringue flavor

--- lista_zakupow2.py
+++ lista_zakupow2.py
@@ -1,8 +1,8 @@
 print("Lista zakupów")
-lista_zakupow = ["Chleb bezglutenowy", "Bułki" ,"Rogaliki","Kokosanki","Beza","Tort"]
+lista_zakupow = ["Chleb bezglutenowy", "Bułki" ,"Rogaliki","Kokosanki","Beza kokosowa","Tort"]
 shopping_dict = {
     "piekarnia" : ["Chleb bezglutenowy", "Bułki" ,"Rogaliki"],
-    "cukiernia" : ["Kokosanki","Beza","Tort"]
+    "cukiernia" : ["Kokosanki","Beza kokosowa","Tort"]
 }
 for key , values in shopping_dict.items():
     print(f"Idę do {key.capitalize()}, kupuję tu : {values}" )
